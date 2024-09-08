b371aae4076cbd08399fdc4429e3f313790b0ba8
saeed-ayman <saeed20359@gmail.com>
2024-09-08 16:42:25 +0300

    try db cache

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Psr\SimpleCache\InvalidArgumentException;
 
 class Image extends Model
 {
@@ -18,13 +19,18 @@ class Image extends Model
         return Image::getUrl($this->attributes['image_id']);
     }
 
+    /**
+     * @throws InvalidArgumentException
+     */
     public static function getUrl(string $image_id): string
     {
-//        if (cache()->has($image_id)) {
-//            return cache()->get($image_id);
-//        }
+        if (\Cache::driver('database')->has($image_id)) {
+            return \Cache::driver('database')->get($image_id);
+        }
 
         $url = \Cloudinary::getUrl($image_id);
+
+        \Cache::driver('database')->put($image_id, $url);
 
         cache()->put($image_id, $url, 86400);
 
