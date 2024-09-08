14a1ff2a7ad9eb3183622537803580bfaa8e578e
saeed-ayman <saeed20359@gmail.com>
2024-09-08 17:18:23 +0300

    try db cache

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -19,20 +19,17 @@ class Image extends Model
         return Image::getUrl($this->attributes['image_id']);
     }
 
-    /**
-     * @throws InvalidArgumentException
-     */
     public static function getUrl(string $image_id): string
     {
-        if (\Cache::driver('database')->has($image_id)) {
-            return \Cache::driver('database')->get($image_id);
+        if (\Cache::has($image_id)) {
+            return \Cache::get($image_id);
         }
 
         $url = \Cloudinary::getUrl($image_id);
 
         \Cache::driver('database')->put($image_id, $url);
 
-        cache()->put($image_id, $url, 86400);
+        \Cache::put($image_id, $url, 86400);
 
         return $url;
     }
